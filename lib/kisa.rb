e57b9b818dd65377148a9ed55d29bc2882733535
S-H-GAMELINKS <gamelinks007@gmail.com>
2025-07-05 16:45:15 +0900

    Support Hashtag Timeline

--- lib/kisa.rb
+++ lib/kisa.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'faraday'
+require 'json'
+require 'cgi'
 require_relative "kisa/version"
 
 class Kisa
@@ -38,7 +40,53 @@ class Kisa
     stream('/api/v1/streaming/public/remote', &block)
   end
 
+  def hashtag_timeline(hashtag, params = {})
+    raise ArgumentError, "hashtag is required" if hashtag.nil? || hashtag.empty?
+
+    # Remove # prefix if present
+    hashtag = hashtag.sub(/^#/, '')
+
+    # Build query parameters
+    query_params = build_query_params(params)
+    url = "/api/v1/timelines/tag/#{hashtag}"
+    url += "?#{query_params}" unless query_params.empty?
+
+    response = @conn.get(url)
+
+    unless response.success?
+      raise Error, "Failed to fetch hashtag timeline: #{response.status} #{response.body}"
+    end
+
+    JSON.parse(response.body)
+  rescue Faraday::ConnectionFailed, Faraday::TimeoutError, Faraday::SSLError
+    raise ConnectionFailedError
+  end
+
   private
+
+  def build_query_params(params)
+    allowed_params = %i[any all none local remote only_media max_id since_id min_id limit]
+    filtered_params = params.select { |key, _| allowed_params.include?(key) }
+
+    query_parts = []
+
+    # Handle array parameters (any, all, none)
+    %i[any all none].each do |param|
+      if filtered_params[param].is_a?(Array)
+        filtered_params[param].each do |value|
+          query_parts << "#{param}[]=#{CGI.escape(value.to_s)}"
+        end
+        filtered_params.delete(param)
+      end
+    end
+
+    # Handle regular parameters
+    filtered_params.each do |key, value|
+      query_parts << "#{key}=#{CGI.escape(value.to_s)}"
+    end
+
+    query_parts.join('&')
+  end
 
   def stream(url)
     unless block_given?
